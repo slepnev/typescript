fe3f92eb9b8ff12d518b08329c1970f38fdc8f06
ayslepnev <ayslepnev@domclick.ru>
2018-08-17 08:28:35 +0300

    ES6: Spread & Destructuring

--- 3_es6.ts
+++ 3_es6.ts
@@ -1,10 +1,16 @@
-// REST & Spread
+// Rest & Spread
 console.log("REST & Spread");
 const number = [1, 10, 99, -5];
 console.log(Math.max(33, 99, -1));
 console.log(Math.max(...number));
 
-function makeArray(...args: number[]) {
+function makeArray(name: string, ...args: number[]) {
   return args;
 }
-console.log(makeArray(1, 2, 3));
+console.log(makeArray("Name", 1, 2, 3));
+
+// Destructuring
+console.log("DESTRUCTURING");
+const myHobbiies = ["Cooking", "Sports"];
+const [hobby1, hobby2] = myHobbiies;
+console.log(hobby1, hobby2);
