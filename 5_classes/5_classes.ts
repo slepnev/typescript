a9f09a360d51c8cc9fe174f762d527f3c43ea093
ayslepnev <ayslepnev@domclick.ru>
2018-09-05 07:39:37 +0300

    ES6: classes

--- 5_classes/5_classes.ts
+++ 5_classes/5_classes.ts
@@ -34,3 +34,57 @@ class Max extends Person {
 }
 const max = new Max("max");
 console.log(max);
+
+// Getter & Setter
+class Plant {
+  private _species: string;
+
+  get species() {
+    return this._species;
+  }
+
+  set species(value: string) {
+    if (value.length > 3) {
+      this._species = value;
+    } else {
+      this._species = "Default";
+    }
+  }
+}
+
+let plant = new Plant();
+console.log(plant.species);
+plant.species = "AB";
+console.log(plant.species);
+plant.species = "Green Plant";
+console.log(plant.species);
+
+// Static Properties & Methods
+class Helpers {
+  static PI: number = 3.14;
+  static calcCircumference(diameter: number): number{
+    return this.PI * diameter;
+  }
+}
+console.log(2 * Helpers.PI);
+console.log(2 * Helpers.calcCircumference(8));
+
+// Abstract Classes
+abstract class Project {
+  projectName: string = "Default";
+  budget: number;
+
+  abstract changeName(name: string): void;
+
+  calcBudget() {
+    return this.budget * 2;
+  }
+}
+
+class ITProject extends Project {
+  changeName(name: string): void {
+    this.projectName = name;
+  }
+}
+
+let newProject = new ITProject();
