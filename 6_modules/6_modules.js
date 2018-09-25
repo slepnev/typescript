55249ee0411f2f1abb8f12e8408d41c3c9e91156
ayslepnev <ayslepnev@domclick.ru>
2018-09-25 20:05:52 +0300

    MODULES: SystemJS

--- 6_modules/6_modules.js
+++ 6_modules/6_modules.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 console.log('NAMESPACE');
 /// <reference path="circleMath.ts" />
 /// <reference path="rectangleMath.ts" />
-console.log(MyMath.calculateRectangle(10, 20));
-console.log(MyMath.calculateCurcumference(3));
+// console.log(MyMath.calculateRectangle(10, 20));
+// console.log(MyMath.calculateCurcumference(3));
 console.log('MODULES');
 var circle_1 = require("./math/circle");
 console.log(circle_1.PI);
+console.log(circle_1.calculateCirumcumference(3));
