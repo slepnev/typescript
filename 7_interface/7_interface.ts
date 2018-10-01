097c256adf4b5009d6623b4dd540dd0b6501087e
ayslepnev <ayslepnev@domclick.ru>
2018-10-01 08:32:36 +0300

    MODULES: Generics

---
+++ 7_interface/7_interface.ts
@@ -0,0 +1,75 @@
+// Interface
+interface NamedPerson {
+  firstName: string;
+  age?: number;
+  [propName: string]: any;
+  greet(lastName: string): void;
+}
+
+function greet(person: NamedPerson) {
+  console.log('Hello, ' + person.firstName);
+}
+
+function changeName(person: NamedPerson) {
+  person.firstName = 'Anna';
+}
+
+const person: NamedPerson = {
+  firstName: 'Max',
+  age: 27,
+  hobbies: ['Cooking'],
+  greet(lastName: string) {
+    console.log('Hi, I am ' + this.firstName + ' ' + lastName);
+  }
+};
+
+person.greet('Anna');
+greet(person);
+changeName(person);
+greet(person);
+
+
+// Class
+class Person implements NamedPerson{
+  firstName: string;
+  lastName: string;
+  age?: number;
+  [propName: string]: any;
+  greet(lastName: string) {
+    console.log('Hi, I am ' + this.firstName + ' ' + lastName);
+  };
+}
+const myPerson = new Person();
+myPerson.firstName = 'Maximilian';
+myPerson.lastName = 'Anything';
+greet(myPerson);
+myPerson.greet(myPerson.lastName);
+
+
+// Function Types
+interface DoubleValueFunc {
+  (number1: number, number2: number): number;
+}
+
+const myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function (value1: number, value2: number) {
+  return (value1 + value2) * 2;
+};
+
+console.log(myDoubleFunction(10, 20));
+
+
+// Interface Inheritance
+interface AgedPerson extends NamedPerson {
+  age: number;
+}
+
+const oldPerson: AgedPerson = {
+  age: 27,
+  firstName: 'Max',
+  greet(lastName: string) {
+    console.log('Hello!');
+  }
+};
+
+console.log(oldPerson);
